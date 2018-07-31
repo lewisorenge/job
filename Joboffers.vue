3b6f774630c7e0cb77779e203fe06bd96a7f8f40
lewisorenge <lewisorenge@gmail.com>
2018-07-31 10:50:11 +0300

    vjs

---
+++ Joboffers.vue
@@ -0,0 +1,132 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12 pull-right no-padding">
+                <div class="vd_content-section clearfix">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="tab-content no-bd mrgn0 pdng0">
+                                <div class="tab-pane active fade in" id="category1">
+                                    <div class="">
+                                        <div class="row mrgn30-0 projectRow no-padmr" style="border-left: 4px solid #80D651;">
+                                            <a href="javascript:void(0);" data-toggle="modal" data-target="#viewThisJob" title="">
+                                                <div class="col-sm-3 brdRight no-padmr" style=" margin-top: 10px;">
+                                                    <div class="col-lg-6 no-padmr"> <img src="img/testi_03.png" alt="" class="projectImg no-padmr"> </div>
+                                                    <div class="col-lg-6 ad-padmr" style="">
+                                                        <p class="mrgn0">User ID:
+                                                            <br><span class="label label-default vd_bg-green vd_white">1122445669</span></p>
+                                                        <p class="mrgn0">User Name: <span class="label label-default vd_bg-green vd_white">Mohamed alaa</span></p>
+                                                    </div>
+                                                </div>
+                                                <div class="col-sm-3 brdRight ">
+                                                    <h4>Design Concept</h4>
+                                                    <p class="mrgn0">Post Time:
+                                                        <br><span class="label label-default vd_bg-blue vd_white">05-07-2017 12:05:00</span></p>
+                                                    <p class="mrgn0">Job Type:
+                                                        <br><span class="label label-default vd_bg-gray vd_white">Offer</span></p>
+                                                </div>
+                                                <div class="col-sm-2 brdRight">
+                                                    <h4>Job Budget</h4>
+                                                    <p class="mrgn0">Budget:
+                                                        <br><span class="label label-default vd_bg-blue vd_white">2.500 €</span> </p>
+                                                    <p class="mrgn0">Type:
+                                                        <br><span class="label label-default vd_bg-gray vd_white">Fix / Hourly</span> </p>
+                                                </div>
+                                                <div class="col-sm-2 brdRight">
+                                                    <h4>Work Start</h4>
+                                                    <p class="mrgn0">Startdate:
+                                                        <br><span class="label label-default vd_bg-blue vd_white">05-07-2017</span></p>
+                                                    <p class="mrgn0">Deliverdate:
+                                                        <br><span class="label label-default vd_bg-green vd_white">05-07-2017</span> </p>
+                                                </div>
+                                                <div class="col-sm-2">
+                                                    <h4><span data-original-title="view proposals" data-toggle="tooltip" data-placement="top">All Tasks</span></h4>
+                                                    <p class="mrgn0"><span class="label label-default vd_bg-red vd_white">1</span></p>
+                                                </div>
+                                            </a>
+                                        </div>
+                                        <div class="row mrgn30-0 projectRow no-padmr" style="border-left: 4px solid #80D651;">
+                                            <a href="javascript:void(0);" data-toggle="modal" data-target="#viewThisJob" title="">
+                                                <div class="col-sm-3 brdRight no-padmr" style=" margin-top: 10px;">
+                                                    <div class="col-lg-6 no-padmr"> <img src="img/testi_03.png" alt="" class="projectImg no-padmr"> </div>
+                                                    <div class="col-lg-6 ad-padmr" style="">
+                                                        <p class="mrgn0">User ID:
+                                                            <br><span class="label label-default vd_bg-green vd_white">1122445669</span></p>
+                                                        <p class="mrgn0">User Name: <span class="label label-default vd_bg-green vd_white">Mohamed alaa</span></p>
+                                                    </div>
+                                                </div>
+                                                <div class="col-sm-3 brdRight ">
+                                                    <h4>Design Concept</h4>
+                                                    <p class="mrgn0">Post Time:
+                                                        <br><span class="label label-default vd_bg-blue vd_white">05-07-2017 12:05:00</span></p>
+                                                    <p class="mrgn0">Job Type:
+                                                        <br><span class="label label-default vd_bg-gray vd_white">Offer</span></p>
+                                                </div>
+                                                <div class="col-sm-2 brdRight">
+                                                    <h4>Job Budget</h4>
+                                                    <p class="mrgn0">Budget:
+                                                        <br><span class="label label-default vd_bg-blue vd_white">2.500 €</span> </p>
+                                                    <p class="mrgn0">Type:
+                                                        <br><span class="label label-default vd_bg-gray vd_white">Fix / Hourly</span> </p>
+                                                </div>
+                                                <div class="col-sm-2 brdRight">
+                                                    <h4>Work Start</h4>
+                                                    <p class="mrgn0">Startdate:
+                                                        <br><span class="label label-default vd_bg-blue vd_white">05-07-2017</span></p>
+                                                    <p class="mrgn0">Deliverdate:
+                                                        <br><span class="label label-default vd_bg-green vd_white">05-07-2017</span> </p>
+                                                </div>
+                                                <div class="col-sm-2">
+                                                    <h4><span data-original-title="view proposals" data-toggle="tooltip" data-placement="top">All Tasks</span></h4>
+                                                    <p class="mrgn0"><span class="label label-default vd_bg-red vd_white">1</span></p>
+                                                </div>
+                                            </a>
+                                        </div>
+                                        <div class="row mrgn30-0 projectRow no-padmr" style="border-left: 4px solid #80D651;">
+                                            <a href="javascript:void(0);" data-toggle="modal" data-target="#viewThisJob" title="">
+                                                <div class="col-sm-3 brdRight no-padmr" style=" margin-top: 10px;">
+                                                    <div class="col-lg-6 no-padmr"> <img src="img/testi_03.png" alt="" class="projectImg no-padmr"> </div>
+                                                    <div class="col-lg-6 ad-padmr" style="">
+                                                        <p class="mrgn0">User ID:
+                                                            <br><span class="label label-default vd_bg-green vd_white">1122445669</span></p>
+                                                        <p class="mrgn0">User Name: <span class="label label-default vd_bg-green vd_white">Mohamed alaa</span></p>
+                                                    </div>
+                                                </div>
+                                                <div class="col-sm-3 brdRight ">
+                                                    <h4>Design Concept</h4>
+                                                    <p class="mrgn0">Post Time:
+                                                        <br><span class="label label-default vd_bg-blue vd_white">05-07-2017 12:05:00</span></p>
+                                                    <p class="mrgn0">Job Type:
+                                                        <br><span class="label label-default vd_bg-gray vd_white">Offer</span></p>
+                                                </div>
+                                                <div class="col-sm-2 brdRight">
+                                                    <h4>Job Budget</h4>
+                                                    <p class="mrgn0">Budget:
+                                                        <br><span class="label label-default vd_bg-blue vd_white">2.500 €</span> </p>
+                                                    <p class="mrgn0">Type:
+                                                        <br><span class="label label-default vd_bg-gray vd_white">Fix / Hourly</span> </p>
+                                                </div>
+                                                <div class="col-sm-2 brdRight">
+                                                    <h4>Work Start</h4>
+                                                    <p class="mrgn0">Startdate:
+                                                        <br><span class="label label-default vd_bg-blue vd_white">05-07-2017</span></p>
+                                                    <p class="mrgn0">Deliverdate:
+                                                        <br><span class="label label-default vd_bg-green vd_white">05-07-2017</span> </p>
+                                                </div>
+                                                <div class="col-sm-2">
+                                                    <h4><span data-original-title="view proposals" data-toggle="tooltip" data-placement="top">All Tasks</span></h4>
+                                                    <p class="mrgn0"><span class="label label-default vd_bg-red vd_white">1</span></p>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+    </div>
+</template>
