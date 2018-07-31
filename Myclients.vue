3043a9f16dd6fa6525c9378e6ada5ec00045327b
lewisorenge <lewisorenge@gmail.com>
2018-07-31 10:50:45 +0300

    vjs

---
+++ Myclients.vue
@@ -0,0 +1,137 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <div class="tab" role="tabpanel">
+                    <div class="tab-content tabs">
+                        <div role="tabpanel" class="tab-pane fade in active" id="CompanyClients">
+                            <div class="pdng20">
+                                <div class="panel widget">
+                                    <div class="">
+                                        <div class="col-lg-4" id="comCL1">
+                                            <div class="ourteamdiver2" id="clListBox">
+                                                <div class="overflowhidde2">
+                                                    <div class="ourteamblur22"> </div>
+                                                </div>
+                                                <hr class="rulerourtema">
+                                                <div class="imgeourteamperson22"> <img src="img/header/logo_2.jpg" style="width:100%;height:100%"> </div>
+                                                <h3 class="ourteamjopposition22 text-center2 col97be2e">Amanda Sun </h3>
+                                                <h3 class="ourteamjopposition222 text-center2">Looking blog writer for our blog </h3>
+                                                <div class="clHover">
+                                                    <div class="col-md-12"> <a class="hover-eye" data-toggle="tooltip" title="" data-original-title="View Detail"><i class="fa fa-eye"></i></a>
+                                                        <button class="hover-eye fa fa-trash" id="deleteClient1" data-toggle="modal" data-target="#deleteClientmodel" title="Remove"></button>
+                                                    </div>
+                                                </div>
+                                                <div style="display: none;" id="clHover2">
+                                                    <div class="col-md-12"> <a class="hover-eye" data-toggle="tooltip" title="" name="CL1" onclick="myFunc(this.name)" data-original-title="YES"><i class="fa fa-check"></i></a> <a class="hover-eye" data-toggle="tooltip" title="" id="noDelete1" data-original-title="NO"><i class="fa fa-times"></i></a> </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-4" id="comCL2">
+                                            <div class="ourteamdiver2" id="clListBox">
+                                                <div class="overflowhidde2">
+                                                    <div class="ourteamblur22"> </div>
+                                                </div>
+                                                <hr class="rulerourtema">
+                                                <div class="imgeourteamperson22"> <img src="img/header/logo_2.jpg" style="width:100%;height:100%"> </div>
+                                                <h3 class="ourteamjopposition22 text-center2 col97be2e">Amanda Sun </h3>
+                                                <h3 class="ourteamjopposition222 text-center2">Looking blog writer for our blog </h3>
+                                                <div class="clHover">
+                                                    <div class="col-md-12"> <a class="hover-eye" data-toggle="tooltip" title="" data-original-title="View Detail"><i class="fa fa-eye"></i></a>
+                                                        <button class="hover-eye fa fa-trash" id="deleteClient2" data-toggle="modal" data-target="#deleteClientmodel" title="Remove"></button>
+                                                    </div>
+                                                </div>
+                                                <div style="display: none;" id="clHover3">
+                                                    <div class="col-md-12"> <a class="hover-eye" data-toggle="tooltip" title="" name="CL2" onclick="myFunc(this.name)" data-original-title="YES"><i class="fa fa-check"></i></a> <a class="hover-eye" data-toggle="tooltip" title="" id="noDelete2" data-original-title="NO"><i class="fa fa-times"></i></a> </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-4" id="comCL3">
+                                            <div class="ourteamdiver2" id="clListBox">
+                                                <div class="overflowhidde2">
+                                                    <div class="ourteamblur22"> </div>
+                                                </div>
+                                                <hr class="rulerourtema">
+                                                <div class="imgeourteamperson22"> <img src="img/header/logo_2.jpg" style="width:100%;height:100%"> </div>
+                                                <h3 class="ourteamjopposition22 text-center2 col97be2e">Amanda Sun </h3>
+                                                <h3 class="ourteamjopposition222 text-center2">Looking blog writer for our blog </h3>
+                                                <div class="clHover">
+                                                    <div class="col-md-12"> <a class="hover-eye" data-toggle="tooltip" title="" data-original-title="View Detail"><i class="fa fa-eye"></i></a>
+                                                        <button class="hover-eye fa fa-trash" id="deleteClient3" data-toggle="modal" data-target="#deleteClientmodel" title="Remove"></button>
+                                                    </div>
+                                                </div>
+                                                <div style="display: none;" id="clHover4">
+                                                    <div class="col-md-12"> <a class="hover-eye" data-toggle="tooltip" title="" name="CL3" onclick="myFunc(this.name)" data-original-title="YES"><i class="fa fa-check"></i></a> <a class="hover-eye" data-toggle="tooltip" title="" id="noDelete3" data-original-title="NO"><i class="fa fa-times"></i></a> </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div role="tabpanel" class="tab-pane fade" id="UserClients">
+                            <div class="panel widget">
+                                <div class="">
+                                    <div class="col-lg-4" id="comCL4">
+                                        <div class="ourteamdiver2" id="clListBox">
+                                            <div class="overflowhidde2">
+                                                <div class="ourteamblur22"> </div>
+                                            </div>
+                                            <hr class="rulerourtema">
+                                            <div class="imgeourteamperson2"> <img src="img/testi_07.png"> </div>
+                                            <h3 class="ourteamjopposition22 text-center2 col97be2e">Amanda Sun </h3>
+                                            <div class="clHover">
+                                                <div class="col-md-12"> <a class="hover-eye" data-toggle="tooltip" title="" data-original-title="View Detail"><i class="fa fa-eye"></i></a>
+                                                    <button class="hover-eye fa fa-trash" id="deleteClient4" data-toggle="modal" data-target="#deleteClientmodel" title="Remove"></button>
+                                                </div>
+                                            </div>
+                                            <div style="display: none;" id="clHover5">
+                                                <div class="col-md-12"> <a class="hover-eye" data-toggle="tooltip" title="" name="CL4" onclick="myFunc(this.name)" data-original-title="YES"><i class="fa fa-check"></i></a> <a class="hover-eye" data-toggle="tooltip" title="" id="noDelete4" data-original-title="NO"><i class="fa fa-times"></i></a> </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-4" id="comCL5">
+                                        <div class="ourteamdiver2" id="clListBox">
+                                            <div class="overflowhidde2">
+                                                <div class="ourteamblur22"> </div>
+                                            </div>
+                                            <hr class="rulerourtema">
+                                            <div class="imgeourteamperson2"> <img src="img/testi_07.png"> </div>
+                                            <h3 class="ourteamjopposition22 text-center2 col97be2e">Amanda Sun </h3>
+                                            <div class="clHover">
+                                                <div class="col-md-12"> <a class="hover-eye" data-toggle="tooltip" title="" data-original-title="View Detail"><i class="fa fa-eye"></i></a>
+                                                    <button class="hover-eye fa fa-trash" id="deleteClient5" data-toggle="modal" data-target="#deleteClientmodel" title="Remove"></button>
+                                                </div>
+                                            </div>
+                                            <div style="display: none;" id="clHover6">
+                                                <div class="col-md-12"> <a class="hover-eye" data-toggle="tooltip" title="" name="CL5" onclick="myFunc(this.name)" data-original-title="YES"><i class="fa fa-check"></i></a> <a class="hover-eye" data-toggle="tooltip" title="" id="noDelete5" data-original-title="NO"><i class="fa fa-times"></i></a> </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-4" id="comCL6">
+                                        <div class="ourteamdiver2" id="clListBox">
+                                            <div class="overflowhidde2">
+                                                <div class="ourteamblur22"> </div>
+                                            </div>
+                                            <hr class="rulerourtema">
+                                            <div class="imgeourteamperson2"> <img src="img/testi_07.png"> </div>
+                                            <h3 class="ourteamjopposition22 text-center2 col97be2e">Amanda Sun </h3>
+                                            <div class="clHover">
+                                                <div class="col-md-12"> <a class="hover-eye" data-toggle="tooltip" title="" data-original-title="View Detail"><i class="fa fa-eye"></i></a>
+                                                    <button class="hover-eye fa fa-trash" id="deleteClient6" data-toggle="modal" data-target="#deleteClientmodel" title="Remove"></button>
+                                                </div>
+                                            </div>
+                                            <div style="display: none;" id="clHover7">
+                                                <div class="col-md-12"> <a class="hover-eye" data-toggle="tooltip" title="" name="CL6" onclick="myFunc(this.name)" data-original-title="YES"><i class="fa fa-check"></i></a> <a class="hover-eye" data-toggle="tooltip" title="" id="noDelete6" data-original-title="NO"><i class="fa fa-times"></i></a> </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
