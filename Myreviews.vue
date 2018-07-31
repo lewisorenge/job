40a8eadd4918addae789c8670d7f32abcb86745b
lewisorenge <lewisorenge@gmail.com>
2018-07-31 10:51:47 +0300

    vjs

---
+++ Myreviews.vue
@@ -0,0 +1,236 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <div class="tab" role="tabpanel">
+                    <div class="tab-content tabs">
+                        <div role="tabpanel" class="tab-pane fade in active" id="Unseenreview">
+                            <div class="col-lg-12" style="width:100%;margin-left:2px; padding:0%;">
+                                <div class="row mrgn30-0 projectRow" data-toggle="modal" data-target="#viewreview" style="padding:0%;" data-title="Edit">
+                                    <a href="javascript:void(0);" title="">
+                                        <div class="col-sm-2" style="padding:0%;"><img style="height:112px;border-left: 5px solid red" src="img/testi_03.png" alt=""></div>
+                                        <div class="col-sm-2" style="margin-top: 10px;">
+                                            <p class="mrgn0">User Name</p>
+                                            <p class="mrgn0">User ID</p>
+                                        </div>
+                                        <div class="col-sm-2 brdRight brdLeft" style="padding-right:10px;padding-left:10px;&nbsp;padding-top: 10px; width: 31%;height: 112px;">
+                                            <h4 style="margin-top: 10px;">Job Title</h4>
+                                            <p>Mohamed alaa is a really skilled freelancer.His skills are above the average. </p>
+                                        </div>
+                                        <!-- <div class="col-sm-1 brdRight" style="&nbsp;padding-top: 10px;width:11%;height: 112px;"> <h4 style="font-size:15px;margin-top: 0;">Options</h4> <p class="mrgn5" data-toggle="modal" data-target="#viewreview" style="margin-bottom: 14px; margin-top: 21px;"><span class="label label-default vd_white" style="6px 15px 6px 13px;font-size:14px;font-family: monospace; border-radius: 4px !important;background:rgba(151, 190, 46, 0.72);">View</span></p></div>-->
+                                        <div class="col-sm-2 " style="padding-left:6%;width:35%; padding-top: 10px;height: 112px;">
+                                            <button type="button" class="btn btn-warning btn-sm pakshj1" aria-label="Left Align"><span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                                            </button>
+                                            <button type="button" class="btn btn-warning btn-sm pakshj1" aria-label="Left Align"><span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                                            </button>
+                                            <button type="button" class="btn btn-warning btn-sm pakshj1" aria-label="Left Align"><span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                                            </button>
+                                            <button type="button" class="btn btn-default btn-grey btn-sm pakshj1" aria-label="Left Align"><span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                                            </button>
+                                            <button type="button" class="btn btn-default btn-grey btn-sm pakshj1" aria-label="Left Align"><span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                                            </button>
+                                            <br>2 months ago
+                                            <br>19 jun,2017
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div role="tabpanel" class="tab-pane fade" id="AllReviews">
+                            <div class="col-lg-10" style="width:100%;padding:0; margin-left: 15px; margin-top:10px; ">
+                                <div class="title" style="margin-bottom: 30px;">
+                                    <div class="companyresume">
+                                        <div class="companyinfo">
+                                            <img width="200" height="30" src="img/testi_10.png" />
+                                            <div class="companytitle">
+                                                Company Name Gmbh
+                                                <br/>
+                                                <br/><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                                <i class="fa fa-star"></i>
+                                            </div>
+                                        </div>
+                                        <div class="companyjobs">Total Jobs
+                                            <br/>
+                                            <br/>150</div>
+                                        <div class="companyreview">Total Review
+                                            <br/>
+                                            <br/>148</div>
+                                        <div class="col-xs-12 col-md-6">
+                                            <div class="well well-sm">
+                                                <div class="row">
+                                                    <div class="col-xs-12 col-md-6 text-center" style="width:33%">
+                                                        <h1 class="rating-num">
+                                                        4.0
+                                                    </h1>
+                                                        <div class="rating">
+                                                            <span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star">
+                                                            </span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star">
+                                                            </span><span class="glyphicon glyphicon-star-empty"></span>
+                                                        </div>
+                                                        <div>
+                                                            <span class="glyphicon glyphicon-user"></span>1,050,008 total
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-xs-12 col-md-6" style="width:30%">
+                                                        <div class="row rating-desc">
+                                                            <div class="col-xs-3 col-md-3 text-right">
+                                                                <span class="glyphicon glyphicon-star"></span>5
+                                                            </div>
+                                                            <div class="col-xs-8 col-md-9">
+                                                                <div class="progress progress-striped">
+                                                                    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+                                                                        <span class="sr-only">80%</span>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <!-- end 5 -->
+                                                            <div class="col-xs-3 col-md-3 text-right">
+                                                                <span class="glyphicon glyphicon-star"></span>4
+                                                            </div>
+                                                            <div class="col-xs-8 col-md-9">
+                                                                <div class="progress">
+                                                                    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+                                                                        <span class="sr-only">60%</span>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <!-- end 4 -->
+                                                            <div class="col-xs-3 col-md-3 text-right">
+                                                                <span class="glyphicon glyphicon-star"></span>3
+                                                            </div>
+                                                            <div class="col-xs-8 col-md-9">
+                                                                <div class="progress">
+                                                                    <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+                                                                        <span class="sr-only">40%</span>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <!-- end 3 -->
+                                                            <div class="col-xs-3 col-md-3 text-right">
+                                                                <span class="glyphicon glyphicon-star"></span>2
+                                                            </div>
+                                                            <div class="col-xs-8 col-md-9">
+                                                                <div class="progress">
+                                                                    <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
+                                                                        <span class="sr-only">20%</span>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <!-- end 2 -->
+                                                            <div class="col-xs-3 col-md-3 text-right">
+                                                                <span class="glyphicon glyphicon-star"></span>1
+                                                            </div>
+                                                            <div class="col-xs-8 col-md-9">
+                                                                <div class="progress">
+                                                                    <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 15%">
+                                                                        <span class="sr-only">15%</span>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <!-- end 1 -->
+                                                        </div>
+                                                        <!-- end row -->
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="companyreviews">
+                                        </div>
+                                    </div>
+                                    <div class="feedback">
+                                        <div class="coment">
+                                            <div class="coment-box">
+                                                <div class="author-image"><img src="img/testi_03.png"></div>
+                                                <div class="rating-section">
+                                                    <div class="rating">
+                                                        <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                                        <i class="fa fa-star-half"></i>
+                                                    </div>
+                                                    <div class="time">
+                                                        <p>2 months ago</p>
+                                                    </div>
+                                                    <div class="date">23,Dec 2016</div>
+                                                </div>
+                                                <div class="description">
+                                                    <div class="title">
+                                                        <h3>Freelance Guru<span class="flag-icon flag-icon-in"><small>india</small></span>
+                                                    </h3>
+                                                        <p>
+                                                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="coment-box">
+                                                <div class="author-image"><img src="img/ashi.jpg"></div>
+                                                <div class="rating-section">
+                                                    <div class="rating">
+                                                        <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                                        <i class="fa fa-star-half"></i>
+                                                    </div>
+                                                    <div class="time">
+                                                        <p>2 months ago</p>
+                                                    </div>
+                                                    <div class="date">23,Dec 2016</div>
+                                                </div>
+                                                <div class="description">
+                                                    <div class="title">
+                                                        <h3>ashish sharma <span class="flag-icon flag-icon-in"><small>india</small></span>
+                                                    </h3>
+                                                        <p>Great Esperience :) </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="coment" style="border-top: #dedede 1px solid; padding-top:15px;">
+                                            <div class="coment-box">
+                                                <div class="author-image"><img src="img/testi_03.png"></div>
+                                                <div class="rating-section">
+                                                    <div class="rating">
+                                                        <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                                        <i class="fa fa-star-half"></i>
+                                                    </div>
+                                                    <div class="time">
+                                                        <p>2 months ago</p>
+                                                    </div>
+                                                    <div class="date">23,Dec 2016</div>
+                                                </div>
+                                                <div class="description">
+                                                    <div class="title">
+                                                        <h3>Work Space <span class="flag-icon flag-icon-in"><small>india</small></span>
+                                                    </h3>
+                                                        <p>Nice Work</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="coment-box">
+                                                <div class="author-image"><img src="img/ashi.jpg"></div>
+                                                <div class="rating-section">
+                                                    <div class="rating">
+                                                        <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                                        <i class="fa fa-star-half"></i>
+                                                    </div>
+                                                    <div class="time">
+                                                        <p>2 months ago</p>
+                                                    </div>
+                                                    <div class="date">23,Dec 2016</div>
+                                                </div>
+                                                <div class="description">
+                                                    <div class="title">
+                                                        <h3>ashish sharma <span class="flag-icon flag-icon-in"><small>india</small></span>
+                                                    </h3>
+                                                        <p>Thanks</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
