51944e2639714cc701d8460e6ed155dc90547f54
lewisorenge <lewisorenge@gmail.com>
2018-07-31 10:54:29 +0300

    ss

---
+++ layouts/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+<footer>
+    <div class="container-fluid footer-content">
+        <div class="pull-left lang">
+            <div class="flag"> <a href="javascript:;" data-target="#langmodal" data-toggle="modal"><span class="flag-icon flag-icon-us"></span> English</a> </div>
+            <a class="help-txt" href="chat.html"><i class="fa fa-question"></i> Help</a>
+        </div>
+        <p class="copyright pull-right">
+            <ul class="list-inline foo_links text-right">
+                <li><a href="index.html#"><i class="fa fa-copyright"></i>&nbsp;ALL RIGHTS RESERVED. COPYRIGHT
+            SmartSystems 2018.</a></li>
+            </ul>
+        </p>
+    </div>
+</footer>
+</template>
