43f7b695b19a3467433ca76607ee9b96d4de13ae
lewisorenge <lewisorenge@gmail.com>
2018-07-31 11:23:26 +0300

    content

--- Jobcontracts.vue
+++ Jobcontracts.vue
@@ -36,7 +36,7 @@
                                             <p class="mrgn5"><span class="label label-default vd_bg-blue vd_white">1</span> Behind</p>
                                         </div>
                                         <div class="col-sm-2">
-                                            <h4 style="font-size:15px;">Release Request</h4>
+                                            <h4 style="font-size:15px;"> Request</h4>
                                             <p class="mrgn5" data-toggle="modal" data-target="#ReleaseRequestsent" style="margin-bottom: 14px;"><span class="label label-default vd_bg-blue vd_white" style="padding:4px;font-size:14px;font-family: monospace; border-radius: 4px !important;">Sent Request</span></p>
                                             <p class="mrgn5" data-toggle="modal" data-target="#ReleaseRequestview"><span class="label label-default vd_bg-blue vd_white" style="padding:4px;font-size:14px;font-family: monospace; border-radius: 4px !important;">View Request</span></p>
                                         </div>
