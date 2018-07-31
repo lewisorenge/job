95809b7487b37b808a5929c9610129fa7d27004b
lewisorenge <lewisorenge@gmail.com>
2018-07-31 10:52:56 +0300

    team

---
+++ Purchasehistory.vue
@@ -0,0 +1,101 @@
+<template>
+      <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12">
+                            <div class="tab" role="tabpanel">
+                                <div class="tab-content tabs">
+                                    <div role="tabpanel" class="tab-pane fade in active" id="ServicePurchase">
+                                        <div class="sales card">
+                                            <div class="card-header card-header-icon" data-background-color="rose">
+                                                <i class="material-icons">assignment</i>
+                                            </div>
+                                            <div class="card-content">
+                                                <h4 class="card-title">Simple Table</h4>
+                                                <div class="table-responsive">
+                                                    <table class="table">
+                                                        <thead>
+                                                            <tr>
+                                                                <th>No</th>
+                                                                <th>Date</th>
+                                                                <th>Service Type</th>
+                                                                <th>Title</th>
+                                                                <th>Price</th>
+                                                                <th>Option</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td></td>
+                                                                <td></td>
+                                                                <td></td>
+                                                                <td></td>
+                                                                <td></td>
+                                                                <td class="td-actions text-right">
+                                                                    <button type="button" rel="tooltip" data-toggle="modal" data-target="#viewThisJob" class="btn btn-preview" data-original-title="" title="">
+                                                                        <i class="fa fa-file-pdf-o"></i>
+                                                                    </button>
+                                                                </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td></td>
+                                                                <td></td>
+                                                                <td></td>
+                                                                <td></td>
+                                                                <td></td>
+                                                                <td class="td-actions text-right">
+                                                                    <button type="button" rel="tooltip" data-toggle="modal" data-target="#viewThisJob" class="btn btn-preview" data-original-title="" title="">
+                                                                        <i class="fa fa-file-pdf-o"></i>
+                                                                    </button>
+                                                                </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td></td>
+                                                                <td></td>
+                                                                <td></td>
+                                                                <td></td>
+                                                                <td></td>
+                                                                <td class="td-actions text-right">
+                                                                    <button type="button" rel="tooltip" data-toggle="modal" data-target="#viewThisJob" class="btn btn-preview" data-original-title="" title="">
+                                                                        <i class="fa fa-file-pdf-o"></i>
+                                                                    </button>
+                                                                </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td></td>
+                                                                <td></td>
+                                                                <td></td>
+                                                                <td></td>
+                                                                <td></td>
+                                                                <td class="td-actions text-right">
+                                                                    <button type="button" rel="tooltip" data-toggle="modal" data-target="#viewThisJob" class="btn btn-preview" data-original-title="" title="">
+                                                                        <i class="fa fa-file-pdf-o"></i>
+                                                                    </button>
+                                                                </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td></td>
+                                                                <td></td>
+                                                                <td></td>
+                                                                <td></td>
+                                                                <td></td>
+                                                                <td class="td-actions text-right">
+                                                                    <button type="button" rel="tooltip" data-toggle="modal" data-target="#viewThisJob" class="btn btn-preview" data-original-title="" title="">
+                                                                        <i class="fa fa-file-pdf-o"></i>
+                                                                    </button>
+                                                                </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div role="tabpanel" class="tab-pane fade in" id="ItemPurchase">
+                                        <h3 class="text-center">Coming soon..</h3>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+</template>
